ef496fbe5dce964fa5c0250b1d169309eae94294
Pradyut Das <daspradyut516@gmail.com>
2023-02-22 13:46:18 +0000

    Update 22 files

---
+++ src/component/bottomNavbar.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import PhoneIcon from "@mui/icons-material/Phone";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+
+export default function IconTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs value={value} onChange={handleChange} sx={{ width: "100vw" }}>
+      <Tab
+        icon={<PhoneIcon />}
+        aria-label="phone"
+        style={{ minWidth: "20%" }}
+      />
+      <Tab
+        icon={<FavoriteIcon />}
+        aria-label="favorite"
+        style={{ minWidth: "20%" }}
+      />
+      <Tab
+        icon={<PersonPinIcon />}
+        aria-label="person"
+        style={{ minWidth: "20%" }}
+      />
+      <Tab
+        icon={<PhoneIcon />}
+        aria-label="phone"
+        style={{ minWidth: "20%" }}
+      />
+      <Tab
+        icon={<FavoriteIcon />}
+        aria-label="favorite"
+        style={{ minWidth: "20%" }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/container/feed.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+
+export default function loginContainer() {
+  
+  return (
+    <main>
+      <Container
+        sx={{
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+          
+      </Container>
+    </main>
+  );
+}
