15c67e88b9eaec28e4dc9a71c6602f4b44c5e032
Pradyut Das <daspradyut516@gmail.com>
2023-03-08 15:52:40 +0000

    Update 4 files

---
+++ src/component/textEditor.js
@@ -0,0 +1,61 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+import dynamic from "next/dynamic";
+
+import "react-quill/dist/quill.snow.css";
+
+const TextEditor = ({ onChange, style }) => {
+  const ReactQuill = React.useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"], // toggled buttons
+      ["blockquote", "code-block"],
+
+      [{ header: 1 }, { header: 2 }], // custom button values
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+      [{ direction: "rtl" }], // text direction
+
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+
+      ["clean"], // remove formatting button
+    ],
+  };
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "formula",
+    "align",
+    "header",
+  ];
+  return (
+    <Box sx={style}>
+      <ReactQuill
+        theme="snow"
+        onChange={onChange}
+        modules={modules}
+        formats={formats}
+      />
+    </Box>
+  );
+};
+
+export default TextEditor;
