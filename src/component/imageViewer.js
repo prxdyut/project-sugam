7191b7068d6badb4deb4efac6038047b8e9aee40
Pradyut Das <daspradyut516@gmail.com>
2023-03-10 18:19:22 +0000

    Update imageViewer.js

--- src/component/imageViewer.js
+++ src/component/imageViewer.js
@@ -1,50 +1,74 @@
 import * as React from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import ImageComponent from "next/image";
 
-function NextJsImage(...[image, , rect]) {
-  const getMeta = (url, cb) => {
-    const img = new Image();
-    img.onload = () => cb(null, img);
-    img.onerror = (err) => cb(err);
-    img.src = url;
-  };
+function getImageDimensions(src) {
+  const [url, setUrl] = React.useState(false);
 
-  // Use like:
-  getMeta(image.src, (err, img) => {
-    const width = Math.round(
-      Math.min(rect.width, (rect.height / img.naturalHeight) * img.naturalWidth)
-    );
-    const height = Math.round(
-      Math.min(rect.height, (rect.width / img.naturalWidth) * img.naturalHeight)
-    );console.log('yeah')
+  const getMeta = (url) =>
+    new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = (err) => reject(err);
+      img.src = url;
+    });
 
-    return (
-      <div style={{ position: "relative", width, height }}>
-        {console.log("0000123456789")}
-        <ImageComponent
-          fill
-          alt=""
-          src={image.src}
-          loading="eager"
-          // layout="fill"
-          // objectFit="contain"
-          // placeholder="blur"
-          draggable={false}
-          sizes={
-            typeof window !== "undefined"
-              ? `${Math.ceil((width / window.innerWidth) * 100)}vw`
-              : `${width}px`
-          }
-        />
-      </div>
+  (async () => {
+    getMeta(src).then(
+      (img) =>
+        !url && setUrl({ width: img.naturalWidth, height: img.naturalHeight })
     );
-  });
+    console.log(url);
+  })();
+  return url;
+}
+
+function NextJsImage(image, offset, rect) {
+  const width = Math.round(
+    Math.min(rect.width, (rect.height / image.height) * image.width)
+  );
+  const height = Math.round(
+    Math.min(rect.height, (rect.width / image.width) * image.height)
+  );
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        fill
+        alt=""
+        src={image}
+        loading="eager"
+        placeholder="blur"
+        draggable={false}
+        sizes={
+          typeof window !== "undefined"
+            ? `${Math.ceil((width / window.innerWidth) * 100)}vw`
+            : `${width}px`
+        }
+      />
+    </div>
+  );
 }
 
 export default function Home({ images }) {
   const [open, setOpen] = React.useState(false);
+  const slides = [
+    {
+      src: "https://picsum.photos/200/300",
+      ...getImageDimensions("https://picsum.photos/200/300"),
+    },
+    {
+      src: "https://picsum.photos/200/400",
+      ...getImageDimensions("https://picsum.photos/200/400"),
+    },
+    {
+      src: "https://picsum.photos/200/350",
+      ...getImageDimensions("https://picsum.photos/200/350"),
+    },
+  ];
+  console.log(slides);
 
   return (
     <main className="flex flex-col items-center text-center p-6 gap-6">
@@ -63,8 +87,9 @@ export default function Home({ images }) {
       <Lightbox
         open={open}
         close={() => setOpen(false)}
-        slides={images}
+        slides={slides}
         render={{ slide: NextJsImage }}
+        plugins={[Zoom]}
       />
     </main>
   );
