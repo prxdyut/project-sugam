1d11764c5a708d899a0eda337cbe8cc16c9b43ec
Pradyut Das <daspradyut516@gmail.com>
2023-03-13 05:40:29 +0000

    Update doubtsNew.js

--- src/container/academics/doubtsNew.js
+++ src/container/academics/doubtsNew.js
@@ -59,7 +59,7 @@ export default function DoubtsNewContainer() {
   console.log(images);
 
   images.map((image, index) =>
-    setImagesArray([...imagesArray, image.base64_file])
+    setImagesArray([...imagesArray, { src: `${image.base64_file}` }])
   );
 
   console.log("Images Array", imagesArray);
