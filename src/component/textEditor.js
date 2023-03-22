ecd48f7f2233b725ede4c8e3b085f715c164696c
Pradyut Das <daspradyut516@gmail.com>
2023-03-22 10:08:42 +0000

    Update widget.js and textEditor.js

--- src/component/textEditor.js
+++ src/component/textEditor.js
@@ -2,27 +2,28 @@ import * as React from "react";
 
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 
 import dynamic from "next/dynamic";
 
 import "react-quill/dist/quill.snow.css";
+
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
   ssr: false,
-  loading: () => <p>Loading ...</p>,
+  loading: () => <Skeleton variant="rounded" height={88 + 18 + 12 * 2} />,
 });
+
 const TextEditor = ({ onChange, style, placeholder, theme }) => {
   const modules = {
     toolbar: [
-      [{ font: [] }],
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["bold", "italic", "underline", "strike"], // toggled buttons
       ["blockquote", "code-block"],
-
       [{ header: 1 }, { header: 2 }], // custom button values
       [{ list: "ordered" }, { list: "bullet" }],
       [{ script: "sub" }, { script: "super" }], // superscript/subscript
       [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-      [{ direction: "rtl" }], // text direction
 
       [{ color: [] }, { background: [] }],
       [{ align: [] }],
